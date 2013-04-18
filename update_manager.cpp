c4aac285144c246cef6ff78f2d6c7469b30bffbf
fancyzero <fancyzero@live.cn>
2013-04-18 23:37:01 +0800

    计算md5

--- update_manager.cpp
+++ update_manager.cpp
@@ -8,6 +8,10 @@
 
 #include "update_manager.h"
 #include "pugixml.hpp"
+#include <sys/stat.h>
+#include "md5.h"
+
+std::string get_file_md5( const std::string& filename );
 
 const file_list_item* file_list::get_file_list_item( const std::string& local_path )
 {
@@ -19,29 +23,46 @@ const file_list_item* file_list::get_file_list_item( const std::string& local_pa
     return NULL;
 }
 
+
+
 bool file_list::load_from_file( const std::string& filename )
 {
     m_list.clear();
     pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load( filename.c_str() );
+    pugi::xml_parse_result result = doc.load_file( filename.c_str() );
     if ( !result )
     {
         return false;
     }
     
-
-    pugi::xml_node info_node = doc.child("filelist").child("info");
-    pugi::xml_node filelist_node = doc.child("filelist").child("files");
+    pugi::xml_node root = doc.child("filelist");
+    if ( root == NULL)
+        return false;
+    pugi::xml_node info_node = root.child("info");
+    pugi::xml_node filelist_node = root.child("files");
     if ( filelist_node == NULL || info_node == NULL )
     {
         return false;
     }
     for (pugi::xml_node file = filelist_node.child("file"); file; file = file.next_sibling("file"))
     {
-        std::cout << file.attribute("url").value();
+        file_list_item new_item;
+        new_item.hash = file.attribute("hash").as_string();
+        new_item.local_path = file.attribute("local_path").as_string();
+        new_item.hash = file.attribute("hash").as_string();
+        new_item.collection = file.attribute("collection").as_string();
     }
     return true;
 }
+
+void file_list::add_item( const file_list_item& item )
+{
+    m_list.push_back( item );
+}
+
+/*
+ update_manager
+ */
 
 update_manager::update_manager()
 {
@@ -50,5 +71,60 @@ update_manager::update_manager()
 
 update_manager::~update_manager()
 {
+    
+}
+
+std::string get_file_md5( const std::string& filename )
+{
+    std::string md5_str = "ffffffffaaaaaaaa";
+    unsigned char digest[MD5_DIGEST_LEN];
+    FILE* pf = fopen( filename.c_str(), "rb" );
+    unsigned char buff[1024];
+    md_context ctx;
+    md5_begin( &ctx );
+    while( !feof(pf) )
+    {
+        size_t readed = fread( buff, 1, 1024, pf);
+        md5_update( &ctx, buff, readed);
+        if ( readed < 1024 )
+            break;
+    }
+    fclose(pf);
+    char cstr_digest[MD5_DIGEST_LEN*2+1];
+    memset(cstr_digest, 0, MD5_DIGEST_LEN*2+1);
+    md5_result( &ctx, digest );
+    for ( int i = 0; i < MD5_DIGEST_LEN; i++ )
+    {
+        sprintf(cstr_digest+i*2, "%02x", digest[i]);
+    }
+    md5_str=cstr_digest;
+    return md5_str;
+}
+
+bool is_identical( const std::string& filename, const std::string& hash )
+{
+    // get local file modified time
+/*struct stat _stat;
+    struct tm* clock;
+    stat( file.c_str(), &_stat);
+    clock = gmtime(&(_stat.st_mtime));
+*/
+    return get_file_md5( filename ) == hash;
+}
+
+file_list update_manager::get_update_list( const std::string& collection )
+{
+    file_list list;
+    for ( FILE_LIST::const_iterator it = m_file_list.m_list.begin(); it != m_file_list.m_list.end(); ++it )
+    {
+        if ( collection.empty() || collection == (*it).collection )
+        {
+            if ( !is_identical( (*it).local_path, (*it).hash) )
+            {
+                list.add_item( *it );
+            }
+        }
+    }
+    return list;
     
 }
--- update_manager.h
+++ update_manager.h
@@ -17,15 +17,17 @@ struct file_list_item
     std::string hash;
     std::string local_path;
     std::string src_url;
+    std::string collection;
 };
 
 typedef std::vector<file_list_item> FILE_LIST;
-class file_list
+struct file_list
 {
 public:
     const file_list_item* get_file_list_item( const std::string& local_path );
     bool load_from_file( const std::string& filename );
-protected:
+    void add_item( const file_list_item& item );
+
     FILE_LIST m_list;
 };
 
@@ -34,6 +36,11 @@ class update_manager
 public:
     update_manager();
     ~update_manager();
+    /*
+     获取某个 文件集合（collection） 的更新列表
+     //如果 collection 为空，则表示包含所有文件
+     */
+    file_list get_update_list( const std::string& collection );
 protected:
     file_list m_file_list;
 };
